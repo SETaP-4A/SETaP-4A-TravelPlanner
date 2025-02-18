8b4ec781b67fed7e6756d096f97adbd6e22664cd
2246702 <up2246702@myport.ac.uk>
2025-02-18 13:49:31 +0000

    initial_ui

---
+++ lib/friends_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FriendsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Friends List',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10),
+          Text('John Doe - Traveling to Rome'),
+          Text('Jane Smith - Exploring Thailand'),
+          Text('Alice Johnson - Backpacking in Peru'),
+        ],
+      ),
+    );
+  }
+}
