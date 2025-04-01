0d1621ae15ce15b7a88dd818b0bdda7790a85fb6
Kevin De Gouveia <degouveiakevin@gmail.com>
2025-04-01 12:45:56 +0100

    removed irrelevant files

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,13 @@
 import 'package:flutter/material.dart';
 import 'package:setap4a/db/database_helper.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'home_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
-
   await DatabaseHelper.initDatabase();
-
   runApp(TravelApp());
 }
 
